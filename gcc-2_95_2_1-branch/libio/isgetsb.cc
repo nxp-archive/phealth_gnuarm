7096c0b2e35700fc39136d156fe5046128df3fa0
(no author) <(no author)@138bc75d-0d04-0410-961f-82ee72b054a4>
1999-10-25 07:23:49 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-2_95_2_1-branch'.
    git-svn-id: svn://gcc.gnu.org/svn/gcc/branches@30162 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc-2_95_2_1-branch/libio/isgetsb.cc
@@ -0,0 +1,4 @@
+      _IO_cleanup_region_start ((void (*) __P ((void *))) _IO_funlockfile,
+				_strbuf);
+      isfx();
+      _IO_cleanup_region_end (0);
---
+++ gcc-2_95_2_1-branch/libio/isscan.cc
@@ -0,0 +1,10 @@
+	_IO_cleanup_region_start ((void (*) __P ((void *))) _IO_funlockfile,
+				  _strbuf);
+	isfx();
+	_IO_cleanup_region_end (0);
+      {
+	_IO_cleanup_region_start ((void (*) __P ((void *))) _IO_funlockfile,
+				  _strbuf);
+	isfx();
+	_IO_cleanup_region_end (0);
+      }
