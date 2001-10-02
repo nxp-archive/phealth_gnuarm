5ca030ef4c7c141291b2c53565bb3082d44832a0
(no author) <(no author)@138bc75d-0d04-0410-961f-82ee72b054a4>
2001-10-02 14:57:59 +0000

    This commit was manufactured by cvs2svn to create branch 'pchmerge-branch'.
    git-svn-id: svn://gcc.gnu.org/svn/gcc/branches@45961 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ pchmerge-branch/libjava/no-threads.cc
@@ -0,0 +1,34 @@
+// no-thread.cc - Implementation of `no threads' threads.
+
+/* Copyright (C) 1998, 1999  Free Software Foundation
+
+   This file is part of libgcj.
+
+This software is copyrighted work licensed under the terms of the
+Libgcj License.  Please consult the file "LIBGCJ_LICENSE" for
+details.  */
+
+#include <config.h>
+
+#include <gcj/cni.h>
+#include <jvm.h>
+#include <java/lang/Thread.h>
+
+java::lang::Thread *_Jv_OnlyThread = NULL;
+
+_Jv_Thread_t *
+_Jv_ThreadInitData (java::lang::Thread * thread)
+{
+  // Don't use JvAssert, since we want this to fail even when compiled
+  // without assertions.
+  if (_Jv_OnlyThread)
+    JvFail ("only thread already running");
+  _Jv_OnlyThread = thread;
+  return NULL;
+}
+
+void
+_Jv_ThreadStart (java::lang::Thread *, _Jv_Thread_t *, _Jv_ThreadStartFunc *)
+{
+  JvFail ("Thread.start called but threads not available");
+}
