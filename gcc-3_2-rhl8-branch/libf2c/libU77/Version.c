981408bef4f8dd0b9d2114851373125e96f45d46
(no author) <(no author)@138bc75d-0d04-0410-961f-82ee72b054a4>
2002-09-23 22:44:53 +0000

    This commit was manufactured by cvs2svn to create branch 'gcc-3_2-rhl8-branch'.
    git-svn-id: svn://gcc.gnu.org/svn/gcc/branches@57454 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc-3_2-rhl8-branch/libf2c/libU77/Version.c
@@ -0,0 +1,12 @@
+static char junk[] = "\n@(#) LIBU77 VERSION 19980709\n";
+
+char __G77_LIBU77_VERSION__[] = "3.2.1 20020903 (prerelease)";
+
+#include <stdio.h>
+
+void
+g77__uvers__ ()
+{
+  fprintf (stderr, "__G77_LIBU77_VERSION__: %s", __G77_LIBU77_VERSION__);
+  fputs (junk, stderr);
+}
