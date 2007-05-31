2d7dd845ca5d4ccdcf97448a46f88c0752376c22
dberlin <dberlin@138bc75d-0d04-0410-961f-82ee72b054a4>
2007-05-31 17:22:43 +0000

    Kill this for rename of fortran-experiments-fix into here
    git-svn-id: svn://gcc.gnu.org/svn/gcc/branches@125233 138bc75d-0d04-0410-961f-82ee72b054a4

--- fortran-experiments/gcc/testsuite/g++.old-deja/g++.brendan/warnings2.C
+++
@@ -1,8 +0,0 @@
-// { dg-do compile  }
-// { dg-options "-Wall -Wextra" }
-// GROUPS passed warnings
-class K {
-public:
-        void f() {
-        }; // there should be no warning about this semicolon
-};
