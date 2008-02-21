894bac690261298c9afa8e8ec485ac26f2913e2f
olga <olga@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-02-21 10:40:50 +0000

    Starting from scratch after gcc4.3 release. The old branch is saved in svn://gcc.gnu.org/svn/gcc/tags/struct-reorg-merge-20080220
    git-svn-id: svn://gcc.gnu.org/svn/gcc/branches@132516 138bc75d-0d04-0410-961f-82ee72b054a4

--- struct-reorg-branch/gcc/testsuite/g++.dg/cpp0x/warn_cxx0x.C
+++
@@ -1,7 +0,0 @@
-// { dg-options "-Wc++0x-compat" }
-int static_assert; // { dg-warning "will become a keyword" }
-
-void foo()
-{
-  static_assert = 5;
-}
--- struct-reorg-branch/gcc/testsuite/gcc.target/i386/xchg-1.c
+++
@@ -1,9 +0,0 @@
-/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
-/* { dg-options "-O2 -march=k8" } */
-
-unsigned short good(unsigned short a)
-{
-       return (a >> 8 | a << 8);
-}
-
-/* { dg-final { scan-assembler "rol" } } */
