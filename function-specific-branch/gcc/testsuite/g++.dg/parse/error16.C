3076d568f2dbdec4b1c75e87efc9eb1c057760bd
meissner <meissner@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-07-23 11:07:17 +0000

    Delete working copy of function-specific to be replaced with a copy of the mainline after it was checked in
    git-svn-id: svn://gcc.gnu.org/svn/gcc/branches@138079 138bc75d-0d04-0410-961f-82ee72b054a4

--- function-specific-branch/gcc/testsuite/g++.dg/parse/error16.C
+++
@@ -1,9 +0,0 @@
-// { dg-options "-fshow-column" }
-// PR c++/16964
-
-struct A
-{
-  struct B {}; // { dg-error "12: error: previous definition of 'struct A::B'" }
-};
-
-struct A::B{}; // { dg-error "11: error: redefinition of 'struct A::B'" }
