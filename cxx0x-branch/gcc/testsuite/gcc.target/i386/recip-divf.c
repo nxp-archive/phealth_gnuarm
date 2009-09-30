d90711022adf12777ae0cee3c2d5a40e4fd671d6
jason <jason@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-09-30 04:57:14 +0000

    delete cxx0x-branch
    git-svn-id: svn://gcc.gnu.org/svn/gcc/branches@152320 138bc75d-0d04-0410-961f-82ee72b054a4

--- cxx0x-branch/gcc/testsuite/gcc.target/i386/recip-divf.c
+++
@@ -1,9 +0,0 @@
-/* { dg-do compile { target i?86-*-* x86_64-*-* } } */
-/* { dg-options "-O2 -ffast-math -msse -mfpmath=sse -mrecip" } */
-
-float t1(float a, float b)
-{
-  return a / b;
-}
-
-/* { dg-final { scan-assembler "rcpss" } } */
